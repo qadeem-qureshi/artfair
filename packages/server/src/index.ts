e84e36fa2ccf25a6c4c9e7d2c99d5036ef071523
Qadeem Qureshi <qadeemqureshi@gmail.com>
2021-09-09 19:42:52 -0500

    Add sockets with a chat message example socket built

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,11 +1,26 @@
 import express from 'express';
 import * as path from 'path';
+import { createServer } from 'http';
+import { Server, Socket } from 'socket.io';
 
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
 const port = process.env.port || 3000;
 const root = path.join(__dirname, '../../client/dist');
+const userHashMap = new Map<string, any>();
 
 app.use(express.static(root));
 
-// eslint-disable-next-line no-console
-app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
+io.on('connection', (_socket: Socket) => {
+  console.log('new client connected!');
+  _socket.on('gameData', (_data) => {
+    userHashMap.set(_socket.id, _data); //  future game data stuff
+  });
+  _socket.on('chatMessage', (_msgPayload: string) => {
+    _socket.broadcast.emit('recieveChatMessage', _socket.id.concat(' ') + _msgPayload); //   send the message (with sender id) to all clients except the sender
+  });
+});
+
+httpServer.listen(port, () => console.log(`App listening at http://localhost:${port}`));
