f9a1b078f75bca739236b9fe555115c395213305
Austin Sands <austinrsands@gmail.com>
2021-09-16 19:01:56 -0500

    Improve responsive design

--- packages/client/src/components/App.tsx
+++ packages/client/src/components/App.tsx
@@ -1,44 +1,58 @@
 import React from 'react';
 import {
-  Box, CssBaseline, makeStyles, ThemeProvider,
+  Box, CssBaseline, makeStyles, ThemeProvider, useMediaQuery,
 } from '@material-ui/core';
+import clsx from 'clsx';
 import Chat from './Chat';
 import Canvas from './Canvas';
 import professional from '../themes/professional';
 
 const useStyles = makeStyles((theme) => ({
-  grid: {
+  content: {
+    padding: '2rem',
     height: '100vh',
-    display: 'grid',
-    gridTemplateColumns: '1fr 1fr 1fr',
-    gridTemplateRows: '1fr 1fr 1fr 1fr',
+    minHeight: '20rem',
+    minWidth: '10rem',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
     gap: '2rem',
   },
+  wrappedContent: {
+    flexDirection: 'column',
+  },
   canvas: {
-    gridArea: '2 / 2 / 3 / 3',
-    placeSelf: 'stretch',
+    height: 'min(70vw, 60vh)',
+    minHeight: '10rem',
+    minWidth: '10rem',
     aspectRatio: '1',
     boxShadow: theme.shadows[3],
     borderRadius: theme.shape.borderRadius,
   },
   chat: {
-    gridArea: '3 / 2 / 4 / 3',
-    placeSelf: 'stretch',
-    minHeight: '15rem',
-    minWidth: '15rem',
+    height: 'min(70vw, 60vh)',
+    width: '20rem',
+    minHeight: '10rem',
+    minWidth: '20rem',
     boxShadow: theme.shadows[3],
     borderRadius: theme.shape.borderRadius,
+  },
+  wrappedChat: {
+    width: 'min(70vw, 60vh)',
+    height: '15rem',
   },
 }));
 
 const App: React.FC = () => {
   const classes = useStyles();
+  const shouldWrap = useMediaQuery('(max-aspect-ratio: 3/2)');
   return (
     <ThemeProvider theme={professional}>
       <CssBaseline />
-      <Box className={classes.grid}>
+      <Box className={clsx(classes.content, shouldWrap && classes.wrappedContent)}>
         <Canvas className={classes.canvas} />
-        <Chat className={classes.chat} />
+        <Chat className={clsx(classes.chat, shouldWrap && classes.wrappedChat)} />
       </Box>
     </ThemeProvider>
   );
